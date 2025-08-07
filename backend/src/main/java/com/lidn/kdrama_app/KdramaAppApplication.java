2a7669c4df74243b0e4704398679777a7fe8bfde
Danny Li <lidn405@gmail.com>
2025-08-06 23:37:18 -0400

    Backend stuff added and name changes to some stuff

---
+++ backend/src/main/java/com/lidn/kdrama_app/KdramaAppApplication.java
@@ -0,0 +1,13 @@
+package com.lidn.kdrama_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdramaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdramaAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lidn/kdrama_app/KdramaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lidn.kdrama_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdramaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
